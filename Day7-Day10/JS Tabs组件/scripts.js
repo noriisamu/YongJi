2e81c56dfab45df48c83cf8db8a0a68fe37a53cb
Ji Yong <1278026146@qq.com>
2018-09-29 17:39:39 +0800

    Day 7 - Day 10 Tabs组件开发

--- Day7-Day10/JS Tabs组件/scripts.js
+++ Day7-Day10/JS Tabs组件/scripts.js
@@ -1,21 +1,50 @@
-window.onload=function(){
-	var items = document.getElementsByTagName("a");
-	var panpel = document.getElementsByClassName("panpel");
-
-	for(var i=0;i<items.length;i++){
-		items[i].classList.add("unSelected");
-	}
-
-	for(let i=0;i<items.length;i++){
-		items[i].onclick=function(){
-			for(var j=0;j<items.length;j++){
-				items[j].classList.remove("isSelected");
-				panpel[j].style.display="none";
-			}
-			items[i].classList.add("isSelected");
-			panpel[i].style.display="block";
-		}
-	}
-	
-
+function Tab(tabId) {
+	this.init(tabId);
 }
+
+Tab.prototype.init = function(tabId) {
+	var tabWrap = document.querySelector(tabId);
+	this.tabs = tabWrap.querySelectorAll('.tab-head li');
+	this.panels = tabWrap.querySelectorAll('.tab-panel-wrap .tab-panel');
+	//记录上一次选择的序号
+	this.current = 0;
+ 
+	this.active(1);
+	this.event();
+};
+ 
+Tab.prototype.active = function(index) {
+	if (index !== this.current) {
+		//显示当前的panepl
+		this.tabs[index].classList.add('active');
+		this.panels[index].classList.add('active');
+		//结束上一次的选择
+		this.tabs[this.current].classList.remove('active');
+		this.panels[this.current].classList.remove('active');
+		//保存
+		this.current = index;
+	}
+};
+ 
+Tab.prototype.event = function() {
+	//保存当前this
+	var tis = this;
+	var len = this.tabs.length;
+ 
+	for (let i = 0; i < len; i++) {
+		//给tab添加监听
+		this.tabs[i].addEventListener ('click', function (event) {
+			tis.active(i);
+		}, false);
+	}
+ 
+};
+ 
+var tab1 = new Tab('#tab1');
+tab1.active(1);
+
+var tab2 = new Tab('#tab2');
+tab2.active(1);
+
+var tab3 = new Tab('#tab3');
+tab2.active(1);
